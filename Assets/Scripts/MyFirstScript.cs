ee7bbbfef33eaa6a151b529713691a6ace413b41
Erik Macháček <dice.sensei@gmail.com>
2023-04-10 18:57:31 +0800

    Add C# test script to project

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private bool hasKey;
+
+    [SerializeField]
+    private string coolName;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
